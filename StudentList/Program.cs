fa8a6df9d044d05e7061563f2ecffff4c9f357d4
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-19 20:39:12 +0900

    클래스 생성

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -8,8 +8,26 @@ namespace StudentList
 {
     class Program
     {
+        class Teacher
+        {
+            public string name;
+            public string major;
+        }
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "함기훈", major="정보 컴퓨터" });
+            list.Add(new Teacher() { name = "이호연", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "정보 컴퓨터" });
+            list.Add(new Teacher() { name = "신혜정", major = "정보 컴퓨터" });
+            list.Add(new Teacher() { name = "김보경", major = "가정" });
+            list.Add(new Teacher() { name = "장민주", major = "디자인" });
+
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.name + " : " + item.major);
+            }
+
         }
     }
 }
